38a2f57ce9a4d18a02b7ac51b9e667b2ec4cd144
Omnivrr <Omnivrr@gmail.com>
2023-04-20 16:36:48 +0300

    Initial Commit

---
+++ Kibristor/KibristorApp.swift
@@ -0,0 +1,17 @@
+//
+//  KibristorApp.swift
+//  Kibristor
+//
+//  Created by Bukhari Sani on 20/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct KibristorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
