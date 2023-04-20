b5a8f8ed664eee4d7156f1764a4b2c8df065afa0
Omnivrr <Omnivrr@gmail.com>
2023-04-21 02:46:18 +0300

    third

--- Kibristor/shoppingStuff.swift
+++ Kibristor/shoppingStuff.swift
@@ -8,27 +8,87 @@
 import SwiftUI
 
 struct shoppingStuff: View {
-    @Binding var selected: String
+    @State var selected = "Home"
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
-
-struct shoppingStuff_Previews: PreviewProvider {
-    static var previews: some View {
-        shoppingStuff()
-    }
-}
-struct CustomTabView: View {
-    var body: some View {
-        HStack {
-            ForEach (tabs, id: \.self) {
-                VStack(spacing: 10) {
-                    Capsule().fill(self.selected == i ? Color("color") : Color.clear).frame(height: 5)
-                    
-                    Button(action: {}, label: <#T##() -> _#>)
+        VStack {
+            if self.selected == "Home" {
+                Home()
+                
+            }
+            else if self.selected == "Wishlist" {
+                GeometryReader{ _ in
+                    Text("Wishlist")
                 }
+            }
+            else {
+                GeometryReader{ _ in
+                    Text("Cart")
+                    
+                }
+            }
+                
+                CustomTabView(selected: $selected)
             }
         }
     }
+    
+    struct shoppingStuff_Previews: PreviewProvider {
+        static var previews: some View {
+            shoppingStuff()
+        }
+    }
+    
+    struct CustomTabView: View {
+        @Binding var selected: String
+        var body: some View {
+            HStack {
+                ForEach (tabs, id: \.self) { i in
+                    VStack(spacing: 10) {
+                        
+                        Capsule()
+                            .fill(Color.clear)
+                            .frame(height: 5)
+                            .overlay(
+                                Capsule().fill(self.selected == i ? Color("color") :
+                                                Color.purple).frame(width: 55, height: 5))
+                        
+                        Button(action: {
+                            self.selected = i
+                        }) {
+                            VStack {
+                                Image(i).renderingMode(.original)
+                                Text(i).foregroundColor(.black)
+                            }
+                        }
+                    }
+                }
+            }.padding(.horizontal)
+        }
+    }
+    
+struct Home: View {
+    @State var text = ""
+    var body: some View {
+        
+        VStack(spacing: 15) {
+            HStack(spacing: 12) {
+                
+                
+                
+                Text("\(Image(systemName: "person.circle"))\t Hello abdulQadeer")
+                Spacer()
+            }
+        }
+        
+        HStack {
+            Image(systemName: "magnifyingglass").font(.body)
+            TextField("Search the store", text: $text)
+        }
+        .padding(10)
+            .background((Color.black.opacity(0.15))
+                .frame(maxWidth: .infinity)
+            .cornerRadius(20))
+    }
 }
+    
+    var tabs = ["Home", "Wishlist", "Cart"]
